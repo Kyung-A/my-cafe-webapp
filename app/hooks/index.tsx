03d6c029a42c4b98b039b9039080a33f093801ea
Nam Eungyeong <nek1717@naver.com>
2024-03-15 23:50:40 +0900

    feat: 현재위치 hook 추가

---
+++ app/hooks/index.tsx
@@ -0,0 +1,1 @@
+export { useGeoLocation } from "./useGeoLocation";
---
+++ app/hooks/useGeoLocation.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+interface ILocation {
+  latitude: number;
+  longitude: number;
+}
+
+export const useGeoLocation = () => {
+  const [location, setLocation] = useState<ILocation>();
+
+  useEffect(() => {
+    const { geolocation } = navigator;
+
+    if (!geolocation) return;
+
+    geolocation.getCurrentPosition(
+      (pos) => {
+        const { latitude, longitude } = pos.coords;
+        setLocation({
+          latitude,
+          longitude,
+        });
+      },
+      (err) => console.error(err)
+    );
+  }, []);
+
+  return {
+    location,
+  };
+};
