bb3de31645704a11c049724fba095a408e910a01
Nam Eungyeong <nek1717@naver.com>
2024-03-17 06:53:57 +0900

    feat: 타입 추가

--- app/shared/types.ts
+++ app/shared/types.ts
@@ -9,3 +9,52 @@ export interface IGeocoder {
   x: number;
   y: number;
 }
+
+export interface ICafeResponse {
+  address_name: string;
+  category_group_code: string;
+  category_group_name: string;
+  category_name: string;
+  distance: string;
+  id: string;
+  phone: string;
+  place_name: string;
+  place_url: string;
+  road_address_name: string;
+  x: string;
+  y: string;
+}
+
+export interface ICafePagination {
+  current: number;
+  first: number;
+  gotoFirst: () => void;
+  gotoLast: () => void;
+  gotoPage: (e: number) => void;
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+  last: number;
+  nextPage: () => void;
+  perPage: number;
+  prevPage: () => void;
+  totalCount: number;
+}
+
+export interface IMarketPostion {
+  La: number;
+  Ma: number;
+}
+
+export interface IMenu {
+  id: string;
+  name: string;
+  href?: string;
+  active: boolean;
+}
+
+export interface IRegister {
+  email: string;
+  name: string;
+  password: string;
+  password2?: string;
+}
