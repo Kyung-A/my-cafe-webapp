c54de6eba5ea4c2cafafba086554ca651db6c082
Nam Eungyeong <nek1717@naver.com>
2024-03-19 04:19:02 +0900

    feat: 데이터 패칭, 리패칭 로직 수정 및 추가

--- app/routes/_app.search.tsx
+++ app/routes/_app.search.tsx
@@ -7,24 +7,17 @@ import {
   useNavigate,
   useOutletContext,
 } from "@remix-run/react";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 import { Card, SearchForm } from "~/components";
-import { useGeoLocation } from "~/hooks";
+import { useClickActive, useGeoLocation } from "~/hooks";
 import { useMap } from "~/shared/contexts/Map";
-import {
-  ICafePagination,
-  ICafeResponse,
-  IGeocoder,
-  IMarketPostion,
-  IRegister,
-} from "~/shared/types";
-import useClickActive from "~/hooks/useClickActive";
+import { ICafeResponse, IGeocoder, IRegister, IReview } from "~/shared/types";
 import { getReviewList } from "~/.server/review";
 
 export async function loader({ request }: { request: Request }) {
   const result = await getReviewList(request);
-  return json({ review: result });
+  return json(result);
 }
 
 export default function CafeSearchRoute() {
@@ -32,120 +25,31 @@ export default function CafeSearchRoute() {
   const userReview = useLoaderData<typeof loader>();
   const { user } = useOutletContext<{ user: IRegister }>();
 
-  const { mapData, lnb, setLnb } = useMap();
+  const {
+    handlePagination,
+    GNB,
+    setGNB,
+    fetchCafeData,
+    cafeData,
+    removeData,
+    removeMarker,
+    refetchCafeData,
+  } = useMap();
   const { location } = useGeoLocation();
   const { handlerActive } = useClickActive();
 
+  const oldReview = useRef<any>(null);
+  const [observerRef, setObserverRef] = useState<null | HTMLDivElement>(null);
   const [address, setAddress] = useState<string>();
-  const [cafeData, setCafeData] = useState<ICafeResponse[]>([]);
-  const [markers, setMarkers] = useState<{ [key: string]: any }[]>([]);
-  const [pagination, setPagination] = useState<ICafePagination>();
 
-  const isActiveLnb = useMemo(() => lnb.find((v) => v.active), [lnb]);
+  const isActiveLnb = useMemo(() => GNB.find((v) => v.active), [GNB]);
 
-  const addMarker = useCallback(
-    (position: IMarketPostion) => {
-      const { kakao } = window;
-      if (!kakao || !mapData) return;
-
-      const imageSrc = "https://t1.daumcdn.net/mapjsapi/images/2x/marker.png";
-      const imageSize = new kakao.maps.Size(28, 40);
-      const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-      const marker = new kakao.maps.Marker({
-        position: position,
-        image: markerImage,
-      });
-      marker.setMap(mapData);
-      return marker;
+  const onScroll = useCallback(
+    (entries: any) => {
+      if (!entries[0].isIntersecting) return;
+      handlePagination();
     },
-    [mapData]
-  );
-
-  const removeMarket = useCallback(() => {
-    if (markers) {
-      markers.forEach((v) => v.setMap(null));
-      setMarkers([]);
-    }
-  }, [markers]);
-
-  const fetchData = useCallback(
-    (type: string) => {
-      const { kakao } = window;
-      if (!kakao || !mapData) return;
-
-      const ps = new kakao.maps.services.Places(mapData);
-      ps.categorySearch(
-        "CE7",
-        (data: ICafeResponse[], status: string, paging: ICafePagination) => {
-          if (status !== kakao.maps.services.Status.OK) return;
-
-          const markerArr: { [key: string]: any }[] = [];
-
-          const result = data.reduce((acc: ICafeResponse[], cur, index) => {
-            userReview.review?.some((reivew, reivewIndex) => {
-              if (cur.id === reivew.cafeId) {
-                if (type === "default") {
-                  acc[index] = {
-                    ...cur,
-                    reviewId: reivew.id,
-                    review: reivew.description,
-                    visited: reivew.visited,
-                    booking: reivew.visited,
-                  };
-                }
-                if (type === "visited" && reivew.visited) {
-                  acc[reivewIndex] = {
-                    ...cur,
-                    reviewId: reivew.id,
-                    review: reivew.description,
-                    visited: reivew.visited,
-                    booking: reivew.booking,
-                  };
-                }
-                if (type === "booking" && reivew.booking) {
-                  acc[reivewIndex] = {
-                    ...cur,
-                    reviewId: reivew.id,
-                    review: reivew.description,
-                    visited: reivew.visited,
-                    booking: reivew.booking,
-                  };
-                }
-              } else {
-                if (type === "default") {
-                  acc[index] = {
-                    ...cur,
-                    visited: false,
-                    booking: false,
-                  };
-                } else if (type === "notVisited") {
-                  acc = [
-                    ...acc,
-                    {
-                      ...cur,
-                      visited: false,
-                      booking: false,
-                    },
-                  ];
-                }
-              }
-            });
-
-            return acc;
-          }, []);
-          result.forEach((cafe) => {
-            const marker = addMarker(new kakao.maps.LatLng(cafe.y, cafe.x));
-            markerArr.push(marker);
-          });
-
-          setPagination(paging);
-          setMarkers(markerArr);
-          setCafeData(result);
-        },
-        { useMapBounds: true }
-      );
-    },
-    [mapData, addMarker, userReview]
+    [handlePagination]
   );
 
   useEffect(() => {
@@ -169,9 +73,81 @@ export default function CafeSearchRoute() {
     });
   }, [location]);
 
+  useEffect(() => {
+    if (!observerRef) return;
+    const observer = new IntersectionObserver(onScroll, { threshold: 0.1 });
+    observer.observe(observerRef);
+
+    if (cafeData.current.length === 45) observer.unobserve(observerRef);
+    return () => observer.disconnect();
+  }, [onScroll, observerRef, cafeData]);
+
+  useEffect(() => {
+    oldReview.current = userReview;
+  }, []);
+
+  useEffect(() => {
+    let changeReview: any;
+    const newIdArr = userReview?.map((v) => v.cafeId);
+    const oldIdArr = oldReview.current?.map((v: IReview) => v.cafeId);
+    const id = newIdArr?.filter((x) => !oldIdArr?.includes(x));
+
+    if (userReview && oldReview.current) {
+      userReview?.forEach((cur) => {
+        oldReview.current.forEach((old: IReview) => {
+          if (cur.cafeId === old.cafeId) {
+            if (cur.description !== old.description) changeReview = cur;
+            if (cur.visited !== old.visited) changeReview = cur;
+            if (cur.booking !== old.booking) changeReview = cur;
+          }
+        });
+        if (id && id.length > 0) {
+          if (cur.cafeId === id[0]) {
+            changeReview = cur;
+          }
+        }
+      });
+    } else {
+      if (userReview && userReview?.length > 0) {
+        changeReview = userReview[0];
+      }
+    }
+
+    if (changeReview) {
+      refetchCafeData(changeReview);
+      oldReview.current = userReview;
+    }
+  }, [refetchCafeData, userReview]);
+
   return (
     <div>
       <div className="bg-primary w-full px-4 py-4">
+        <div className="mb-2 flex items-center gap-2 text-white">
+          <button
+            onClick={() => {
+              navigate("/search");
+              setGNB(GNB.map((v) => ({ ...v, active: false })));
+              removeData();
+              removeMarker();
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+          <h1>myCafe</h1>
+        </div>
         <SearchForm />
       </div>
       <div>
@@ -179,7 +155,7 @@ export default function CafeSearchRoute() {
           <div className="px-4 pb-40 pt-6">
             <h2 className="text-lg font-semibold">☕ {address} 주변 탐색</h2>
             <ul className="mt-3 flex flex-col gap-2">
-              {lnb?.map((v) => (
+              {GNB?.map((v) => (
                 <li key={v.id}>
                   <button
                     type="button"
@@ -188,7 +164,7 @@ export default function CafeSearchRoute() {
                         navigate("/signin");
                       } else {
                         handlerActive(v.id);
-                        fetchData(v.id);
+                        fetchCafeData(v.id, userReview);
                       }
                     }}
                     className={`border-primary w-full rounded border px-4 py-2 text-left ${v.active ? "bg-interaction border-interaction font-semibold text-white" : ""}`}
@@ -202,87 +178,6 @@ export default function CafeSearchRoute() {
         ) : (
           <>
             <div className="px-4 pb-2 pt-6">
-              <div className="flex items-center justify-between">
-                <button
-                  onClick={() => {
-                    navigate("/search");
-                    setLnb(lnb.map((v) => ({ ...v, active: false })));
-                    removeMarket();
-                    setCafeData([]);
-                  }}
-                  className="flex items-center gap-1"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-4 w-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
-                    />
-                  </svg>
-                  <p className="text-xs">뒤로가기</p>
-                </button>
-                <div className="flex items-center gap-2">
-                  <button
-                    onClick={() => {
-                      if (pagination?.hasPrevPage) {
-                        pagination?.gotoPage(pagination.current - 1);
-                        removeMarket();
-                        setCafeData([]);
-                      }
-                    }}
-                    className={`text-interaction flex items-center gap-2 ${pagination?.hasPrevPage ? "text-interaction" : "pointer-events-none text-neutral-400"}`}
-                  >
-                    <p className="text-xs">이전 보기</p>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="h-4 w-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
-                      />
-                    </svg>
-                  </button>
-                  <button
-                    onClick={() => {
-                      if (pagination?.hasNextPage) {
-                        pagination?.gotoPage(pagination.current + 1);
-                        removeMarket();
-                        setCafeData([]);
-                      }
-                    }}
-                    className={`text-interaction flex items-center gap-2 ${pagination?.hasNextPage ? "text-interaction" : "pointer-events-none text-neutral-400"}`}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="h-4 w-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
-                      />
-                    </svg>
-                    <p className="text-xs">다음 보기</p>
-                  </button>
-                </div>
-              </div>
               <h2 className="text-md mt-1 font-semibold leading-6">
                 {address} 주변 <br />
               </h2>
@@ -290,23 +185,28 @@ export default function CafeSearchRoute() {
                 {isActiveLnb.name}
               </h3>
             </div>
-            <div className="h-screen w-full overflow-y-auto px-4 pb-[300px]">
-              <ul className="mt-2 flex flex-col gap-6">
-                {cafeData?.map((v) => (
-                  <Link
-                    to={v.id}
-                    key={v.id}
-                    state={{ review: v.review, reviewId: v.reviewId }}
-                  >
-                    <Card data={v} />
-                  </Link>
-                ))}
+            <div className="h-screen w-full overflow-y-auto px-4 pb-[200px]">
+              <ul className="mt-2 flex min-h-[500px] flex-col gap-6">
+                {cafeData.current && (
+                  <>
+                    {cafeData.current.map((v: ICafeResponse) => (
+                      <Link
+                        to={v.id}
+                        key={v.id}
+                        state={{ review: v.review, reviewId: v.reviewId }}
+                      >
+                        <Card data={v} />
+                      </Link>
+                    ))}
+                  </>
+                )}
               </ul>
+              <div ref={setObserverRef} className="h-1"></div>
             </div>
           </>
         )}
       </div>
-      <Outlet />
+      <Outlet context={userReview} />
     </div>
   );
 }
--- app/shared/contexts/Map.tsx
+++ app/shared/contexts/Map.tsx
@@ -6,16 +6,23 @@ import {
   useRef,
   MutableRefObject,
   useState,
+  useCallback,
 } from "react";
 import { useGeoLocation } from "~/hooks";
-import { lnb } from "../consts/tabs";
-import { IMenu } from "../types";
+import { GNB } from "../consts/gnb";
+import { ICafePagination, ICafeResponse, IMenu, IReview } from "../types";
 
 interface IMap {
   mapEl: MutableRefObject<any> | null;
   mapData: any;
-  lnb: IMenu[];
-  setLnb: React.Dispatch<React.SetStateAction<IMenu[]>>;
+  GNB: IMenu[];
+  setGNB: React.Dispatch<React.SetStateAction<IMenu[]>>;
+  cafeData: any;
+  fetchCafeData: (type: string, data: any) => void;
+  handlePagination: () => void;
+  removeData: () => void;
+  removeMarker: () => void;
+  refetchCafeData: (reivew: IReview) => void;
 }
 
 interface IMapProvider {
@@ -25,17 +32,212 @@ interface IMapProvider {
 const MapContext = createContext<IMap>({
   mapEl: null,
   mapData: {},
-  lnb: [],
-  setLnb: () => [],
+  GNB: [],
+  setGNB: () => [],
+  cafeData: [],
+  fetchCafeData: () => null,
+  handlePagination: () => null,
+  removeData: () => null,
+  removeMarker: () => null,
+  refetchCafeData: () => null,
 });
 
 const MapProvider = ({ children }: IMapProvider) => {
   const mapEl = useRef(null);
+  const cafeData = useRef<ICafeResponse[]>([]);
   const { location } = useGeoLocation();
 
   const [mapData, setMapData] = useState();
-  const [copyLnb, setCopyLnb] = useState<IMenu[]>(lnb);
+  const [copyGNB, setCopyGNB] = useState<IMenu[]>(GNB);
+  const [markers, setMarkers] = useState<{ [key: string]: any }[]>([]);
+  const [pagination, setPagination] = useState<ICafePagination>();
 
+  // 지도 마커 삭제
+  const removeMarker = useCallback(() => {
+    markers.forEach((v) => v.setMap(null));
+    setMarkers([]);
+  }, [markers]);
+
+  // 카페 목록 삭제
+  const removeData = () => {
+    cafeData.current = [];
+  };
+
+  // 목록 다음 페이지
+  const handlePagination = useCallback(() => {
+    if (pagination?.hasNextPage) {
+      removeMarker();
+      pagination?.gotoPage(pagination.current + 1);
+    }
+  }, [pagination]);
+
+  // 마커 추가
+  const addMarker = useCallback(
+    (data: ICafeResponse[] | null) => {
+      const { kakao } = window;
+      if (!kakao || !mapData) return;
+
+      const markerArr: { [key: string]: any }[] = [];
+
+      data?.forEach((cafe) => {
+        const position = new kakao.maps.LatLng(cafe.y, cafe.x);
+
+        const imageSrc = "https://t1.daumcdn.net/mapjsapi/images/2x/marker.png";
+        const imageSize = new kakao.maps.Size(28, 40);
+        const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+        const marker = new kakao.maps.Marker({
+          map: mapData,
+          position: position,
+          image: markerImage,
+        });
+        markerArr.push(marker);
+      });
+
+      setMarkers(markerArr);
+    },
+    [mapData]
+  );
+
+  // 후기 등록, 수정 시 목록 리패칭
+  const refetchCafeData = (newReview: IReview) => {
+    cafeData.current.forEach((v, i) => {
+      if (v.id === newReview.cafeId) {
+        cafeData.current[i] = {
+          ...v,
+          review: newReview.description,
+          visited: newReview.visited,
+          booking: newReview.booking,
+        };
+      }
+    });
+  };
+
+  // 카페 데이터와 유저 리뷰 데이터 맵핑
+  const fetchCafeData = useCallback(
+    (type: string, reviewData: IReview[]) => {
+      const { kakao } = window;
+      if (!kakao || !mapData) return;
+
+      const ps = new kakao.maps.services.Places(mapData);
+      ps.categorySearch(
+        "CE7",
+        (data: ICafeResponse[], status: string, paging: ICafePagination) => {
+          if (status !== kakao.maps.services.Status.OK) return;
+
+          /* 카페 목록 */
+          let result: ICafeResponse[] = [];
+
+          if (type !== "notVisited") {
+            result = data.reduce((acc: ICafeResponse[], cur, index) => {
+              // 리뷰 X && 주변검색
+              if (!reviewData && type === "default") {
+                acc[index] = {
+                  ...cur,
+                  visited: false,
+                  booking: false,
+                };
+              }
+
+              // 리뷰 O
+              if (reviewData) {
+                // 주변검색
+                if (type === "default") {
+                  for (const review of reviewData) {
+                    const { id, cafeId, description, visited, booking } =
+                      review;
+
+                    if (cur.id === cafeId) {
+                      acc[index] = {
+                        ...cur,
+                        reviewId: id,
+                        review: description,
+                        visited: visited,
+                        booking: booking,
+                      };
+                      break;
+                    } else {
+                      // 리뷰 있/없 총 데이터 반환
+                      acc[index] = {
+                        ...cur,
+                        visited: false,
+                        booking: false,
+                      };
+                    }
+                  }
+                }
+
+                // 방문 O
+                if (type === "visited") {
+                  reviewData.forEach((review) => {
+                    const { id, cafeId, description, visited, booking } =
+                      review;
+                    if (cur.id === cafeId) {
+                      if (visited) {
+                        acc.push({
+                          ...cur,
+                          reviewId: id,
+                          review: description,
+                          visited: visited,
+                          booking: booking,
+                        });
+                      }
+                    }
+                  });
+                }
+
+                // 즐겨찾기
+                if (type === "booking") {
+                  reviewData.forEach((review) => {
+                    const { id, cafeId, description, visited, booking } =
+                      review;
+                    if (cur.id === cafeId) {
+                      if (booking) {
+                        acc.push({
+                          ...cur,
+                          ...(id && { reviewId: id }),
+                          ...(description && {
+                            review: description,
+                          }),
+                          visited: visited,
+                          booking: booking,
+                        });
+                      }
+                    }
+                  });
+                }
+              }
+
+              return acc;
+            }, []);
+          } else {
+            if (reviewData) {
+              result = data.filter((v) => {
+                return !reviewData.some(
+                  (review) =>
+                    review.cafeId === v.id && {
+                      ...v,
+                      visited: false,
+                      booking: review.booking,
+                    }
+                );
+              });
+            }
+          }
+
+          cafeData.current = [...cafeData.current, ...result];
+          setPagination(paging);
+
+          if (markers.length === 0) {
+            addMarker(cafeData.current);
+          }
+        },
+        { useMapBounds: true }
+      );
+    },
+    [addMarker, mapData, markers]
+  );
+
+  // 지도 초기화
   useEffect(() => {
     const { kakao } = window;
     if (!mapEl.current || !kakao || !location) return;
@@ -44,7 +246,7 @@ const MapProvider = ({ children }: IMapProvider) => {
     kakao.maps.load(() => {
       const options = {
         center: new kakao.maps.LatLng(latitude, longitude),
-        level: 5,
+        level: 6,
       };
 
       const map = new kakao.maps.Map(mapEl.current, options);
@@ -55,10 +257,16 @@ const MapProvider = ({ children }: IMapProvider) => {
   return (
     <MapContext.Provider
       value={{
-        mapEl: mapEl,
-        mapData: mapData,
-        lnb: copyLnb,
-        setLnb: setCopyLnb,
+        mapEl,
+        mapData,
+        GNB: copyGNB,
+        setGNB: setCopyGNB,
+        cafeData: cafeData,
+        fetchCafeData,
+        handlePagination,
+        removeData,
+        removeMarker,
+        refetchCafeData,
       }}
     >
       {children}
