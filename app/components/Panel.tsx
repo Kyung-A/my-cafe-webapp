dc531b58e224150942a7b1498c445279131f3d05
Nam Eungyeong <nek1717@naver.com>
2024-04-11 15:06:57 +0900

    feat: 패널 열기, 닫기 구현

--- app/components/Panel.tsx
+++ app/components/Panel.tsx
@@ -1,23 +1,24 @@
-import chevronRight from "~/assets/chevronRight.svg";
-
 export const Panel = ({
   children,
-  left,
+  left = "0px",
+  open,
 }: {
   children: React.ReactNode;
   left?: string;
+  open?: boolean;
 }) => {
   return (
     <div
-      className="absolute top-0 z-10 h-full w-80 shadow-[4px_-1px_9px_0px_#52525230]"
-      style={{ left: left ?? "0px" }}
+      className="absolute top-0 z-10 h-full w-80 shadow-[4px_-1px_9px_0px_#52525230] transition-all"
+      style={{
+        left: open
+          ? location.pathname === "/search"
+            ? "-320px"
+            : "-640px"
+          : left,
+      }}
     >
       <div className="h-full w-full bg-white">{children}</div>
-      <button className="absolute -right-5 top-1/2 -mt-6 h-12 w-5">
-        <div className="flex h-full w-full items-center justify-center bg-white">
-          <img src={chevronRight} className="w-4" alt="패널여닫" />
-        </div>
-      </button>
     </div>
   );
 };
--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -1,9 +1,11 @@
 import { MetaFunction, json } from "@remix-run/node";
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { Outlet, useLoaderData, useLocation } from "@remix-run/react";
+import { useState } from "react";
 import { getUser } from "~/.server/storage";
 
 import { Panel } from "~/components";
 import { useMap } from "~/shared/contexts/Map";
+import chevronRight from "~/assets/chevronRight.svg";
 
 export const meta: MetaFunction = () => {
   return [{ title: "myCafe" }];
@@ -16,13 +18,28 @@ export async function loader({ request }: { request: Request }) {
 }
 
 export default function MainLayoutRoute() {
+  const [isPanelOpen, setPanelOpen] = useState(false);
+
+  const location = useLocation();
   const user = useLoaderData<typeof loader>();
   const { mapEl } = useMap();
 
   return (
     <div ref={mapEl} id="map" className="relative h-screen w-full">
-      <Panel>
+      <Panel open={isPanelOpen}>
         <Outlet context={{ user }} />
+        <button
+          onClick={() => setPanelOpen((prev) => !prev)}
+          className={`absolute top-1/2 z-10 -mt-6 h-12 w-5 ${location.pathname === "/search" ? "-right-5" : "-right-[340px]"}`}
+        >
+          <div className="flex h-full w-full items-center justify-center bg-white">
+            <img
+              src={chevronRight}
+              className={`w-4 ${isPanelOpen ? "" : "rotate-180"}`}
+              alt="패널여닫"
+            />
+          </div>
+        </button>
       </Panel>
     </div>
   );
