8f00e36667d046c1cd46200eb8cda1bcb06e1569
Nam Eungyeong <nek1717@naver.com>
2024-05-08 04:04:35 +0900

    fix: 마커 버그 수정

--- app/routes/_app.search.tsx
+++ app/routes/_app.search.tsx
@@ -120,7 +120,7 @@ export default function CafeSearchRoute() {
         fetchCafeData(id, userReview as IReview[]);
       }
     },
-    [navigate, user, userReview, mapData]
+    [navigate, user, userReview, mapData, location]
   );
 
   useEffect(() => {
