021ee3f3e9719073de81b8cfee18addc8c93919d
Nam Eungyeong <nek1717@naver.com>
2024-05-11 02:49:10 +0900

    배포 에러 테스트

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,16 @@
-import { redirect } from "@remix-run/react";
+import type { MetaFunction } from "@remix-run/node";
 
-export const loader = async () => redirect("/search");
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Welcome to Remix</h1>
+    </div>
+  );
+}
