d4fa36a59d542a1cbd5753e67e07d7440c49b331
Nam Eungyeong <nek1717@naver.com>
2024-09-05 02:51:40 +0900

    feat: searchForm 리팩토링

--- app/shared/ui/SearchForm.tsx
+++ app/shared/ui/SearchForm.tsx
@@ -1,51 +1,31 @@
-import { useEffect, useRef } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { IMenu, IReview } from "~/shared/types";
+import { IReview } from "~/shared/types";
 
 interface ISearch {
   searchInput: string;
   setSearchInput: React.Dispatch<React.SetStateAction<string>>;
-  handleEnter: (
-    e: { key: string },
-    text: string,
-    userReview: IReview[]
-  ) => void;
-  onSubmit: (text: string, userReview: IReview[]) => void;
-  isActiveMenu: IMenu | undefined;
+  handleInteraction: (e: any, text: string) => void;
   userReview: IReview[];
 }
 
 export function SearchForm({
   searchInput,
   setSearchInput,
-  handleEnter,
-  onSubmit,
-  isActiveMenu,
-  userReview,
+  handleInteraction,
 }: ISearch) {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
-  useEffect(() => {
-    if (inputRef.current) {
-      if (!isActiveMenu || isActiveMenu.id !== "search") {
-        inputRef.current.value = "";
-      }
-    }
-  }, [isActiveMenu, inputRef]);
-
   return (
     <div className="flex w-full items-center justify-between overflow-hidden rounded bg-white">
       <input
-        ref={inputRef}
         type="search"
         onChange={(e) => setSearchInput(e.target.value)}
-        onKeyDown={(e) => handleEnter(e, searchInput, userReview)}
+        onKeyDown={(e) => handleInteraction(e, searchInput)}
         placeholder="찾으시는 카페가 있으신가요?"
         className="box-border w-[90%] border-none p-2 text-base outline-none placeholder:text-base placeholder:text-zinc-400"
       />
       <button
         type="button"
-        onClick={() => onSubmit(searchInput, userReview)}
+        onClick={(e) => handleInteraction(e, searchInput)}
         className="p-2"
       >
         <MagnifyingGlassIcon className="w-5 text-gray-400" />
