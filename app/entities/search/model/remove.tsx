bd4cd4295cd5a923136db8161291b37aec41d97d
Nam Eungyeong <nek1717@naver.com>
2024-09-05 03:35:49 +0900

    feat: remove 로직 분리

---
+++ app/entities/search/model/remove.tsx
@@ -0,0 +1,39 @@
+import { Dispatch, SetStateAction } from "react";
+import { IClusterer, IMarker, ISearchData } from "../types";
+
+export function removeMarker(
+  markers: IMarker[] | undefined,
+  setMarkers: Dispatch<SetStateAction<IMarker[] | undefined>>
+) {
+  markers?.forEach((v: IMarker) => v.setMap(null));
+  setMarkers([]);
+}
+
+export function removeCafeData(cafeData: { current: ISearchData[] }) {
+  cafeData.current = [];
+}
+
+export function removewOverlay(overlay: IMarker[], listOverlayArr: IMarker[]) {
+  overlay.forEach((v) => {
+    v.setMap(null);
+  });
+  listOverlayArr[0]?.setMap(null);
+}
+
+export function removeClusterer(clusterer: IClusterer | undefined) {
+  clusterer?.clear();
+}
+
+export function allRemove(
+  markers: IMarker[] | undefined,
+  setMarkers: Dispatch<SetStateAction<IMarker[] | undefined>>,
+  cafeData: { current: ISearchData[] },
+  overlay: IMarker[],
+  listOverlayArr: IMarker[],
+  clusterer: IClusterer | undefined
+) {
+  removeMarker(markers, setMarkers);
+  removeCafeData(cafeData);
+  removewOverlay(overlay, listOverlayArr);
+  removeClusterer(clusterer);
+}
